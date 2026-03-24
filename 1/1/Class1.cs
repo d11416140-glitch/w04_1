22d7af0f7d477e758597c22d119ff61fd3b8ee09
d11416140-glitch <d11416140@gs.takming.edu.tw>
2026-03-24 13:53:42 +0800

    Add files via upload

---
+++ 1/1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class test
+{
+    private int mid;
+    private const int final=99;
+
+
+    public int mymid { get; } = 56;
+    public int myfinal
+    {
+        get
+        {
+            return final;
+
+        }
+        set
+        {
+            if (value > 0 && value < 100)
+            {
+                //final = value;
+            }
+            else
+            {
+                //final = 0;
+            }
+        }
+    }
+}
+
---
+++ 1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            test std = new test();
+
+            
+            std.myfinal = 78;
+
+            Console.WriteLine(std.mymid.ToString());
+            Console.WriteLine(std.myfinal.ToString());
+
+
+        }
+    }
+}
